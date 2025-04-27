c7f364f412f977606b8fe38f4ca2f272e3e4afd8
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-04-27 09:42:19 -0400

    save

--- kernel/dev.bash
+++ kernel/dev.bash
@@ -2,12 +2,20 @@
 # for bootstrapping a kernel build before make.bash fully works
 set -e
 set -x
-# uncomment next line if generated kernel cannot be used
-cp @golden-kernel0d.py kernel0d.py
-# comment next line if generated kernel cannot be used
-#cat external.py stock.py 0d.py >kernel0d.py
 
 node das2json.js kernel.drawio
+
+OLD=1
+if [ "$OLD" -eq 1 ]; then
+    cp @golden-kernel0d.py kernel0d.py
+    python3 backward.py
+    mv modified-kernel.drawio.json kernel.drawio.json
+else
+    cat external.py stock.py 0d.py >kernel0d.py
+fi
+
+# comment next line if generated kernel cannot be used
+
 ./tas.bash 'external'
 ./tas.bash 'stock'
 ./tas.bash '0d'
