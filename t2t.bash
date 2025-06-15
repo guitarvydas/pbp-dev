88d3c511bdd78b96b32e34b5c7d3e8377c29b4e5
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-06-15 07:28:50 -0400

    convert backticks to string concatenations

--- t2t.bash
+++ t2t.bash
@@ -9,6 +9,6 @@ src=$6
 lib=${pbp}/t2t/lib
 
 node "${lib}/rwr.mjs" "${rewrite}" >"${wd}/temp.rewrite.mjs"
-sed -e 's/`/\\`/g <${grammar} >temp.grammar
+sed -e 's/`/` + "`" + String.raw`/g' <${grammar} >temp.grammar
 cat "${lib}/front.part.js" temp.grammar "${lib}/middle.part.js" "${lib}/args.part.js" "${support}" "${wd}/temp.rewrite.mjs" "${lib}/tail.part.js" >"${wd}/temp.nanodsl.mjs"
 node "${wd}/temp.nanodsl.mjs" "${src}"
